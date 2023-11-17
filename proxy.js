d07e939dc28ce936c7e20a0ee76ce808edb7ef77
0xCosydell <david.awatere@gmail.com>
2023-11-17 11:20:04 +0100

    add middleware and post logic

--- proxy.js
+++ proxy.js
@@ -2,27 +2,46 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+
+// Middleware to parse JSON body for POST requests
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-// Proxy endpoint
+// Proxy endpoint for GET requests
 app.get('/api/proxy', async (req, res) => {
     try {
-        // Access the API_KEY from environment variables
         const apiKey = process.env.API_KEY;
-
-        // Construct the URL with the API key
         const url = `https://oklyglwabkhjmbmxsdga.supabase.co/rest/v1/relays?apikey=${apiKey}`;
 
-        // Make a request to the SB API
         const response = await axios.get(url);
 
         // Transform the SB response to match the MV structure
         const transformedResponse = response.data['relay-data']['0'];
 
-        // Send the transformed response back to the client
         res.json(transformedResponse);
     } catch (error) {
-        // Handle errors (e.g., network issues, transformation errors)
+        res.status(500).send('Error while processing request');
+    }
+});
+
+// New POST proxy endpoint for /v1/submit-voucher
+app.post('/v1/submit-voucher', async (req, res) => {
+    try {
+        const apiKey = process.env.API_KEY;
+        const url = 'https://api.mullvad.net/v1/submit-voucher';
+
+        const postData = req.body; // e.g., { "voucher_code": "ABCD-EFGH-1234-5678" }
+
+        const response = await axios.post(url, postData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
         res.status(500).send('Error while processing request');
     }
 });

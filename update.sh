06d867f1bc922108818d82c3eb3a1fa1878df7ff
0xCosydell <david.awatere@gmail.com>
2023-11-27 21:43:13 +1300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Update api server by cloning the current configuration on gh then restart the api daemon
+# Author: David Awatere
+# update.sh
 
 # Define the URLs and paths
 repository_url="https://github.com/hinetapora/api-proxy"
 app_directory="/home/unblockvpnio/api-proxy"
 clone_directory="/home/unblockvpnio/tmp"
+
+# Copy this script to a temporary location
+cp "$0" "$clone_directory/update.sh"
 
 # Clone the Git repository
 git clone "$repository_url" "$clone_directory"
@@ -22,10 +28,13 @@ if [ $? -eq 0 ]; then
     # Delete the temporary directory
     rm -rf "$clone_directory"
     echo "Temporary directory deleted."
+    
+    # Restart the PM2 process
+    sudo pm2 restart "$app_directory/proxy.js" --watch --log
+    echo "PM2 process restarted."
   else
     echo "Error: Failed to copy files to app folder."
   fi
 else
   echo "Error: Git clone failed."
 fi
-

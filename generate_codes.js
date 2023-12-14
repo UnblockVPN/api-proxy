0615bd0e2d61d8a307da5bd2fb21af67d19a168f
0xCosydell <david.awatere@gmail.com>
2023-12-14 18:11:12 +0100

    Update generate_codes.js

--- generate_codes.js
+++ generate_codes.js
@@ -6,46 +6,42 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 const voucherTable = 'vouchers';
 
 // Function to generate a random voucher code
 function generateRandomVoucherCode() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let code = '';
-  for (let i = 0; i < 16; i++) {
-    if (i % 4 === 0 && i !== 0) {
-      code += '-';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+    for (let i = 0; i < 16; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters.charAt(randomIndex);
     }
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    code += characters.charAt(randomIndex);
+    return code;
   }
-  return code;
-}
-
-// Function to insert voucher codes
-async function insertVoucherCodes() {
-  try {
-    const voucherCodes = [];
-    const numberOfCodesToGenerate = 90; // Change this to the desired number of codes
-
-    for (let i = 0; i < numberOfCodesToGenerate; i++) {
-      const voucherCode = generateRandomVoucherCode();
-      voucherCodes.push({ code: voucherCode, duration_months: 3 }); // Set the duration as needed
+  
+  // Function to insert voucher codes
+  async function insertVoucherCodes() {
+    try {
+      const voucherCodes = [];
+      const numberOfCodesToGenerate = 10; // Change this to the desired number of codes
+  
+      for (let i = 0; i < numberOfCodesToGenerate; i++) {
+        const voucherCode = generateRandomVoucherCode();
+        voucherCodes.push({ code: voucherCode, duration_months: 3 }); // Set the duration as needed
+      }
+  
+      const { data, error } = await supabase
+        .from(voucherTable)
+        .upsert(voucherCodes, { onConflict: ['code'], returning: ['code'] });
+  
+      if (error) {
+        console.error('Error inserting voucher codes:', error);
+      } else {
+        console.log('Successfully inserted voucher codes:', data);
+      }
+    } catch (error) {
+      console.error('Error inserting voucher codes:', error.message);
     }
-
-    const { data, error } = await supabase
-      .from(voucherTable)
-      .upsert(voucherCodes, { onConflict: ['code'], returning: ['code'] });
-
-    if (error) {
-      console.error('Error inserting voucher codes:', error);
-    } else {
-      console.log('Successfully inserted voucher codes:', data);
-    }
-  } catch (error) {
-    console.error('Error inserting voucher codes:', error.message);
   }
-}
-
-insertVoucherCodes();
+  
+  insertVoucherCodes();
